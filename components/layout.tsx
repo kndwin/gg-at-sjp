4cb70c4092411cbd42e4feddd881670e82f72479
Kevin Nguyen <me@kndwin.dev>
2020-10-20 00:48:24 +1100

    🔨 fixed the site title to be correct

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = `Growth Groups at St John's`
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'GG@SJP'
 
 export default function Layout({
   children,
